a60f105d2ad949e8b53f63c6d3a0db9418830e39
dbr <dbr.onix@gmail.com>
2009-06-27 18:55:16 +0100

    Added exceptions file

---
+++ tvnamer_exceptions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#encoding:utf-8
+#author:dbr/Ben
+#project:tvnamer
+#repository:http://github.com/dbr/tvnamer
+#license:Creative Commons GNU GPL v2
+# http://creativecommons.org/licenses/GPL/2.0/
+
+"""Exceptions used through-out tvnamer
+"""
+
+class BaseTvnamerException(Exception):
+    """Base exception all tvnamers exceptions inherit from
+    """
+    pass
+
+class InvalidPath(BaseTvnamerException):
+    """Raised when an argument is a non-existent file or directory path
+    """
+    pass
+
+
+class NoValidFilesFoundError(BaseTvnamerException):
+    """Raised when no valid files are found. Effectively exits tvnamer
+    """
+    pass
+
+class InvalidFilename(BaseTvnamerException):
+    """Raised when a file is parsed, but no episode info can be found
+    """
+    pass
