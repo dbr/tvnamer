6a517662b759006dd9a266a354d4c618306de228
dbr <dbr.onix@gmail.com>
2009-07-21 22:11:29 +0930

    Typos in test_files, blah_blah to blahblah

--- tests/test_files.py
+++ tests/test_files.py
@@ -13,72 +13,72 @@ files = {}
 
 files['default_format'] = [
     {'input': 'Scrubs - [04x19] - My Best Laid Plans',
-    'season_number': 4, 'episode_number': 19
+    'seasonnumber': 4, 'episodenumber': 19
     },
-    {'input': 'Scrus - [02x11].avi',
-    'season_number': 2, 'episode_number': 11
+    {'input': 'Scrus - [02x11]',
+    'seasonnumber': 2, 'episodenumber': 11
     },
 ]
 
 files['s01e01_format'] = [
-    {'input': 'scrubs.s01e01.avi',
-    'season_number': 1, 'episode_number': 1
+    {'input': 'scrubs.s01e01',
+    'seasonnumber': 1, 'episodenumber': 1
     },
-    {'input': 'my.name.is.earl.s01e01.avi',
-    'season_number': 1, 'episode_number': 1
+    {'input': 'my.name.is.earl.s01e01',
+    'seasonnumber': 1, 'episodenumber': 1
     },
-    {'input': 'scrubs.s0e24.blah.fake.avi',
-    'season_number': 1, 'episode_number': 1
+    {'input': 'scrubs.s0e24.blah.fake',
+    'seasonnumber': 1, 'episodenumber': 1
     },
 ]
 
 files['multiple_episodes'] = [
-    {'input': 'scrubs.s01e23e24.avi',
-    'season_number': 1, 'episode_number': [23, 24]
+    {'input': 'scrubs.s01e23e24',
+    'seasonnumber': 1, 'episodenumber': [23, 24]
     },
-    {'input': 'Stargate SG-1 - [01x01-02].avi',
-    'season_number': 1, 'episode_number': [1, 2]
+    {'input': 'Stargate SG-1 - [01x01-02]',
+    'seasonnumber': 1, 'episodenumber': [1, 2]
     },
-    {'input': '[Lunar] Bleach - 52-53 [B937F496].avi',
-    'season_number': None, 'episode_number': [52, 53]
+    {'input': '[Lunar] Bleach - 52-53 [B937F496]',
+    'seasonnumber': None, 'episodenumber': [52, 53]
     },
-    {'input': 'scrubs.s01e01e02e03.avi',
-    'season_number': 1, 'episode_number': [1, 2, 3]
+    {'input': 'scrubs.s01e01e02e03',
+    'seasonnumber': 1, 'episodenumber': [1, 2, 3]
     },
-    {'input': 'Scrubs - [02x01-03].avi',
-    'season_number': 2, 'episode_number': [1, 2, 3]
+    {'input': 'Scrubs - [02x01-03]',
+    'seasonnumber': 2, 'episodenumber': [1, 2, 3]
     },
 ]
 
 files['unicode'] = [
-    {'input': 'DARKER THAN BLACK -\xe9\xbb\x92\xe3\x81\xae\xe5\xa5\x91\xe7\xb4\x84\xe8\x80\x85- - S01E01 (21st copy).mkv',
-    'season_number': 1, 'episode_number': 1
+    {'input': 'DARKER THAN BLACK -\xe9\xbb\x92\xe3\x81\xae\xe5\xa5\x91\xe7\xb4\x84\xe8\x80\x85- - S01E01 (21st copy)',
+    'seasonnumber': 1, 'episodenumber': 1
     },
-    {'input': 'Carniv\xc3\xa0le 1x11 - The Day of the Dead.avi',
-    'season_number': 1, 'episode_number': 11
+    {'input': 'Carniv\xc3\xa0le 1x11 - The Day of the Dead',
+    'seasonnumber': 1, 'episodenumber': 11
     },
-    {'input': 't\xc3\xacnh ng\xc6\xb0\xe1\xbb\x9di hi\xe1\xbb\x87n \xc4\x91\xe1\xba\xa1i - [01x01].avi',
-    'season_number': 1, 'episode_number': 1
+    {'input': 't\xc3\xacnh ng\xc6\xb0\xe1\xbb\x9di hi\xe1\xbb\x87n \xc4\x91\xe1\xba\xa1i - [01x01]',
+    'seasonnumber': 1, 'episodenumber': 1
     },
 ]
 
 files['anime'] = [
-    {'input': '[Eclipse] Fullmetal Alchemist Brotherhood - 02 (1280x720 h264) [8452C4BF].mkv',
-    'season_number': None, 'episode_number': 2
-    }
-    {'input': '[Shinsen-Subs] Armored Trooper Votoms - Pailsen Files - 01 [9E3F1D1C].avi',
-    'season_number': None, 'episode_number': 1
-    }
-    {'input': '[Shinsen-Subs] Beet - 19 [24DAB497].avi',
-    'season_number': None, 'episode_number': 19
-    }
-    {'input': '[AG-SHS]Victory_Gundam-03_DVD[FC6E3A6F].avi',
-    'season_number': None, 'episode_number': 3
-    }
-    {'input': '[YuS-SHS]Gintama-88(H264)_[52CA4F8B].mp4',
-    'season_number': None, 'episode_number': 88
-    }
-    {'input': '[Shinsen-Subs] True Mazinger - 07 [848x480 H.264 Vorbis][787D0074].mkv',
-    'season_number': None, 'episode_number': 7
+    {'input': '[Eclipse] Fullmetal Alchemist Brotherhood - 02 (1280x720 h264) [8452C4BF]',
+    'seasonnumber': None, 'episodenumber': 2
+    },
+    {'input': '[Shinsen-Subs] Armored Trooper Votoms - Pailsen Files - 01 [9E3F1D1C]',
+    'seasonnumber': None, 'episodenumber': 1
+    },
+    {'input': '[Shinsen-Subs] Beet - 19 [24DAB497]',
+    'seasonnumber': None, 'episodenumber': 19
+    },
+    {'input': '[AG-SHS]Victory_Gundam-03_DVD[FC6E3A6F]',
+    'seasonnumber': None, 'episodenumber': 3
+    },
+    {'input': '[YuS-SHS]Gintama-88(H264)_[52CA4F8B]',
+    'seasonnumber': None, 'episodenumber': 88
+    },
+    {'input': '[Shinsen-Subs] True Mazinger - 07 [848x480 H.264 Vorbis][787D0074]',
+    'seasonnumber': None, 'episodenumber': 7
     }
 ]
