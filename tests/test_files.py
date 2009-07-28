b497f0e31a4f4e3fae93b48ed4ac0ea62e0757ab
dbr <dbr.onix@gmail.com>
2009-07-28 23:18:13 +0930

    Added new test case, added seriesname check to all test cases

--- tests/test_files.py
+++ tests/test_files.py
@@ -13,72 +13,78 @@ files = {}
 
 files['default_format'] = [
     {'input': 'Scrubs - [04x19] - My Best Laid Plans',
-    'seasonnumber': 4, 'episodenumber': 19
-    },
+    'seriesname': 'scrubs',
+    'seasonnumber': 4, 'episodenumber': 19},
     {'input': 'Scrus - [02x11]',
-    'seasonnumber': 2, 'episodenumber': 11
-    },
+    'seriesname': 'scrubs',
+    'seasonnumber': 2, 'episodenumber': 11},
 ]
 
 files['s01e01_format'] = [
     {'input': 'scrubs.s01e01',
-    'seasonnumber': 1, 'episodenumber': 1
-    },
+    'seriesname': 'scrubs',
+    'seasonnumber': 1, 'episodenumber': 1},
     {'input': 'my.name.is.earl.s01e01',
-    'seasonnumber': 1, 'episodenumber': 1
-    },
+    'seriesname': 'my name is earl',
+    'seasonnumber': 1, 'episodenumber': 1},
     {'input': 'scrubs.s01e24.blah.fake',
-    'seasonnumber': 1, 'episodenumber': 24
-    },
+    'seriesname': 'scrubs',
+    'seasonnumber': 1, 'episodenumber': 24},
+]
+
+files['misc'] = [
+    {'input': 'Six.Feet.Under.S0201.test_testing-yay',
+    'seriesname': 'six feet under',
+    'seasonnumber': 2, 'episodenumber': 1}
 ]
 
 files['multiple_episodes'] = [
     {'input': 'scrubs.s01e23e24',
-    'seasonnumber': 1, 'episodenumber': [23, 24]
-    },
+    'seriesname': 'scrubs',
+    'seasonnumber': 1, 'episodenumber': [23, 24]},
     {'input': 'Stargate SG-1 - [01x01-02]',
-    'seasonnumber': 1, 'episodenumber': [1, 2]
-    },
+    'seriesname': 'stargate sg-1',
+    'seasonnumber': 1, 'episodenumber': [1, 2]},
     {'input': '[Lunar] Bleach - 52-53 [B937F496]',
-    'seasonnumber': None, 'episodenumber': [52, 53]
-    },
+    'seriesname': 'bleach',
+    'seasonnumber': None, 'episodenumber': [52, 53]},
     {'input': 'scrubs.s01e01e02e03',
-    'seasonnumber': 1, 'episodenumber': [1, 2, 3]
-    },
+    'seriesname': 'scrubs',
+    'seasonnumber': 1, 'episodenumber': [1, 2, 3]},
     {'input': 'Scrubs - [02x01-03]',
-    'seasonnumber': 2, 'episodenumber': [1, 2, 3]
-    },
+    'seriesname': 'scrubs',
+    'seasonnumber': 2, 'episodenumber': [1, 2, 3]},
 ]
 
 files['unicode'] = [
     {'input': 'DARKER THAN BLACK -\xe9\xbb\x92\xe3\x81\xae\xe5\xa5\x91\xe7\xb4\x84\xe8\x80\x85- - S01E01 (21st copy)',
-    'seasonnumber': 1, 'episodenumber': 1
-    },
+    'seriesname': 'darker than black',
+    'seasonnumber': 1, 'episodenumber': 1},
     {'input': 'Carniv\xc3\xa0le 1x11 - The Day of the Dead',
-    'seasonnumber': 1, 'episodenumber': 11
-    },
+    'seriesname': 'Carniv\xc3\xa0le',
+    'seasonnumber': 1, 'episodenumber': 11},
     {'input': 't\xc3\xacnh ng\xc6\xb0\xe1\xbb\x9di hi\xe1\xbb\x87n \xc4\x91\xe1\xba\xa1i - [01x01]',
-    'seasonnumber': 1, 'episodenumber': 1
-    },
+    'seriesname': 't\xc3\xacnh ng\xc6\xb0\xe1\xbb\x9di hi\xe1\xbb\x87n \xc4\x91\xe1\xba\xa1i',
+    'seasonnumber': 1, 'episodenumber': 1},
 ]
 
 files['anime'] = [
     {'input': '[Eclipse] Fullmetal Alchemist Brotherhood - 02 (1280x720 h264) [8452C4BF]',
-    'seasonnumber': None, 'episodenumber': 2
-    },
+    'seriesname': 'fullmetal alchemist brotherhood',
+    'seasonnumber': None, 'episodenumber': 2},
     {'input': '[Shinsen-Subs] Armored Trooper Votoms - Pailsen Files - 01 [9E3F1D1C]',
-    'seasonnumber': None, 'episodenumber': 1
-    },
+    'seriesname': 'armored trooper votoms - pailsen files',
+    'seasonnumber': None, 'episodenumber': 1},
     {'input': '[Shinsen-Subs] Beet - 19 [24DAB497]',
-    'seasonnumber': None, 'episodenumber': 19
-    },
+    'seriesname': 'beet',
+    'seasonnumber': None, 'episodenumber': 19},
     {'input': '[AG-SHS]Victory_Gundam-03_DVD[FC6E3A6F]',
-    'seasonnumber': None, 'episodenumber': 3
-    },
+    'seriesname': 'victory gundam',
+    'seasonnumber': None, 'episodenumber': 3},
     {'input': '[YuS-SHS]Gintama-88(H264)_[52CA4F8B]',
-    'seasonnumber': None, 'episodenumber': 88
-    },
+    'seriesname': 'gintama',
+    'seasonnumber': None, 'episodenumber': 88},
     {'input': '[Shinsen-Subs] True Mazinger - 07 [848x480 H.264 Vorbis][787D0074]',
-    'seasonnumber': None, 'episodenumber': 7
-    }
+    'seriesname': 'true mazinger',
+    'seasonnumber': None, 'episodenumber': 7},
 ]
