b1369a0160b56f66750ddd5c235d36445f4979ef
dbr <dbr.onix@gmail.com>
2009-09-03 18:28:27 +0100

    Clean up test_config, remove temp file after test, remove unused import

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,32 +11,34 @@
 
 import os
 import sys
-import unittest
 import tempfile
 
 sys.path.append(os.path.join(os.path.abspath(sys.path[0]), ".."))
 from config import Config
 
+
 def test_saving_loading():
     """Tests saving the default config to a temp folder, and reloading it
     """
-    
+
     # Save config
     fid, fname = tempfile.mkstemp()
     Config.useDefaultConfig()
     Config.saveConfig(fname)
     assert len(open(fname).read()) > 10, "Config file is less than 10 characters long"
-    
+
     # Make copy of config dict
     saved_config = dict(Config)
-    
+
     # Clear config
     Config.loadConfig(fname)
     new_config = dict(Config)
-    
+
     # Compare
-    
     assert saved_config == new_config, "Configs do not match"
+    
+    # Clear config
+    os.close(fid)
 
 if __name__ == '__main__':
     import nose
