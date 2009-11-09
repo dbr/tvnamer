0a36dc3147aff1e730baba374476739a29ba6b44
dbr <dbr.onix@gmail.com>
2009-11-09 23:26:32 +0000

    Rewrite test_name_parser using nosetest's test-generation, so I can move the various formats into test_files

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -9,7 +9,6 @@
 """Test tvnamer's filename parser
 """
 
-import unittest
 from helpers import assertEquals
 
 from tvnamer.utils import FileParser
@@ -17,90 +16,90 @@ from tvnamer.utils import FileParser
 from test_files import files
 
 
-class test_name_parser(unittest.TestCase):
+def test_autogen_names():
+    """Tests set of standard filename formats with various data
+    """
 
-    def setUp(self):
-        """Define name formats to test.
-        %(seriesname)s becomes the seriesname,
-        %(seasno)s becomes the season number,
-        %(epno)s becomes the episode number.
-        """
+    """Mostly based on scene naming standards:
+    http://tvunderground.org.ru/forum/index.php?showtopic=8488
 
-        #scene naming standards: http://tvunderground.org.ru/forum/index.php?showtopic=8488
-        self.name_formats = [
-            '%(seriesname)s.s%(seasno)de%(epno)d.dsr.nf.avi',                 #seriesname.s01e02.dsr.nf.avi
-            '%(seriesname)s.S%(seasno)dE%(epno)d.PROPER.dsr.nf.avi',          #seriesname.S01E02.PROPER.dsr.nf.avi
-            '%(seriesname)s.s%(seasno)d.e%(epno)d.avi',                       #seriesname.s01.e02.avi
-            '%(seriesname)s-s%(seasno)de%(epno)d.avi',                        #seriesname-s01e02.avi
-            '%(seriesname)s-s%(seasno)de%(epno)d.the.wrong.ep.name.avi',      #seriesname-s01e02.the.wrong.ep.name.avi
-            '%(seriesname)s - [%(seasno)dx%(epno)d].avi',                     #seriesname - [01x02].avi
-            '%(seriesname)s - [%(seasno)dx0%(epno)d].avi',                    #seriesname - [01x002].avi
-            '%(seriesname)s-[%(seasno)dx%(epno)d].avi',                       #seriesname-[01x02].avi
-            '%(seriesname)s [%(seasno)dx%(epno)d].avi',                       #seriesname [01x02].avi
-            '%(seriesname)s [%(seasno)dx%(epno)d] the wrong ep name.avi',     #seriesname [01x02] epname.avi
-            '%(seriesname)s [%(seasno)dx%(epno)d] - the wrong ep name.avi',   #seriesname [01x02] - the wrong ep name.avi
-            '%(seriesname)s - [%(seasno)dx%(epno)d] - the wrong ep name.avi', #seriesname - [01x02] - the wrong ep name.avi
-            '%(seriesname)s.%(seasno)dx%(epno)d.The_Wrong_ep_name.avi',       #seriesname.01x02.epname.avi
-            '%(seriesname)s.%(seasno)d%(epno)02d.The Wrong_ep.names.avi',     #seriesname.102.epname.avi
-            '%(seriesname)s_s%(seasno)de%(epno)d_The_Wrong_ep_na-me.avi',     #seriesname_s1e02_epname.avi
-            '%(seriesname)s - s%(seasno)de%(epno)d - dsr.nf.avi',             #seriesname - s01e02 - dsr.nf.avi
-            '%(seriesname)s - s%(seasno)de%(epno)d - the wrong ep name.avi',  #seriesname - s01e02 - the wrong ep name.avi
-            '%(seriesname)s - s%(seasno)de%(epno)d - the wrong ep name.avi',  #seriesname - s01e02 - the_wrong_ep_name!.avi
-        ]
+    %(seriesname)s becomes the seriesname,
+    %(seasno)s becomes the season number,
+    %(epno)s becomes the episode number.
 
-    def test_name_parser_basic(self):
-        """Tests most basic filename (simple seriesname)
-        """
-        name_data = {'seriesname': 'series name'}
-        self._run_test(name_data)
+    Each is string-formatted with seasons from 0 to 10, and ep 0 to 10
+    """
 
-    def test_name_parser_showdashname(self):
-        """Tests with dash in seriesname
-        """
-        name_data = {'seriesname': 'S-how name'}
-        self._run_test(name_data)
+    name_formats = [
+        '%(seriesname)s.s%(seasno)de%(epno)d.dsr.nf.avi',                 # seriesname.s01e02.dsr.nf.avi
+        '%(seriesname)s.S%(seasno)dE%(epno)d.PROPER.dsr.nf.avi',          # seriesname.S01E02.PROPER.dsr.nf.avi
+        '%(seriesname)s.s%(seasno)d.e%(epno)d.avi',                       # seriesname.s01.e02.avi
+        '%(seriesname)s-s%(seasno)de%(epno)d.avi',                        # seriesname-s01e02.avi
+        '%(seriesname)s-s%(seasno)de%(epno)d.the.wrong.ep.name.avi',      # seriesname-s01e02.the.wrong.ep.name.avi
+        '%(seriesname)s - [%(seasno)dx%(epno)d].avi',                     # seriesname - [01x02].avi
+        '%(seriesname)s - [%(seasno)dx0%(epno)d].avi',                    # seriesname - [01x002].avi
+        '%(seriesname)s-[%(seasno)dx%(epno)d].avi',                       # seriesname-[01x02].avi
+        '%(seriesname)s [%(seasno)dx%(epno)d].avi',                       # seriesname [01x02].avi
+        '%(seriesname)s [%(seasno)dx%(epno)d] the wrong ep name.avi',     # seriesname [01x02] epname.avi
+        '%(seriesname)s [%(seasno)dx%(epno)d] - the wrong ep name.avi',   # seriesname [01x02] - the wrong ep name.avi
+        '%(seriesname)s - [%(seasno)dx%(epno)d] - the wrong ep name.avi', # seriesname - [01x02] - the wrong ep name.avi
+        '%(seriesname)s.%(seasno)dx%(epno)d.The_Wrong_ep_name.avi',       # seriesname.01x02.epname.avi
+        '%(seriesname)s.%(seasno)d%(epno)02d.The Wrong_ep.names.avi',     # seriesname.102.epname.avi
+        '%(seriesname)s_s%(seasno)de%(epno)d_The_Wrong_ep_na-me.avi',     # seriesname_s1e02_epname.avi
+        '%(seriesname)s - s%(seasno)de%(epno)d - dsr.nf.avi',             # seriesname - s01e02 - dsr.nf.avi
+        '%(seriesname)s - s%(seasno)de%(epno)d - the wrong ep name.avi',  # seriesname - s01e02 - the wrong ep name.avi
+        '%(seriesname)s - s%(seasno)de%(epno)d - the wrong ep name.avi',  # seriesname - s01e02 - the_wrong_ep_name!.avi
+    ]
 
-    def test_name_parser_shownumeric(self):
-        """Tests with numeric show name
-        """
-        name_data = {'seriesname': '123'}
-        self._run_test(name_data)
+    test_data = [
+    {'name': 'test_name_parser_unicode',
+    'description': 'Tests parsing show containing unicode characters',
+    'name_data': {'seriesname': 'T\xc3\xacnh Ng\xc6\xb0\xe1\xbb\x9di Hi\xe1\xbb\x87n \xc4\x90\xe1\xba\xa1i'}},
 
-    def test_name_parser_shownumericspaces(self):
-        """Tests with numeric show name, with spaces
-        """
-        name_data = {'seriesname': '123 2008'}
-        self._run_test(name_data)
+    {'name': 'test_name_parser_basic',
+    'description': 'Tests most basic filename (simple seriesname)',
+    'name_data': {'seriesname': 'series name'}},
 
-    def test_name_parser_exclaim(self):
-        """Tests parsing show with explaimation mark
-        """
-        name_data = {'seriesname': 'Show name!'}
-        self._run_test(name_data)
+    {'name': 'test_name_parser_showdashname',
+    'description': 'Tests with dash in seriesname',
+    'name_data': {'seriesname': 'S-how name'}},
 
-    def test_name_parser_unicode(self):
-        """Tests parsing show containing unicode characters"""
-        name_data = {'seriesname': u'T\xecnh Ng\u01b0\u1eddi Hi\u1ec7n \u0110\u1ea1i'.encode('utf-8')}
-        self._run_test(name_data)
+    {'name': 'test_name_parser_exclaim',
+    'description': 'Tests parsing show with explaimation mark',
+    'name_data': {'seriesname': 'Show name!'}},
 
-    def _run_test(self, name_data):
-        """Runs the tests and checks if the parsed values have
-        the correct seriesname/season number/episode number.
-        Runs from season 0 ep 0 to season 10, ep 10.
-        """
-        for seas in xrange(1, 11):
-            for ep in xrange(1, 11):
-                name_data['seasno'] = seas
-                name_data['epno'] = ep
+    {'name': 'test_name_parser_shownumeric',
+    'description': 'Tests with numeric show name',
+    'name_data': {'seriesname': '123'}},
 
-                names = [x % name_data for x in self.name_formats]
+    {'name': 'test_name_parser_shownumericspaces',
+    'description': 'Tests with numeric show name, with spaces',
+    'name_data': {'seriesname': '123 2008'}},
+    ]
 
-                for cur in names:
-                    p = FileParser(cur).parse()
+    for cdata in test_data:
+        # Make new wrapped function
+        def cur_test():
+            for seas in xrange(1, 11):
+                for ep in xrange(1, 11):
 
-                    self.assertEquals(p.episodenumber, name_data['epno'])
-                    self.assertEquals(p.seasonnumber, name_data['seasno'])
-                    self.assertEquals(p.seriesname, name_data['seriesname'])
+                    name_data = cdata['name_data']
+
+                    name_data['seasno'] = seas
+                    name_data['epno'] = ep
+
+                    names = [x % name_data for x in name_formats]
+
+                    for cur in names:
+                        p = FileParser(cur).parse()
+
+                        assertEquals(p.episodenumber, name_data['epno'])
+                        assertEquals(p.seasonnumber, name_data['seasno'])
+                        assertEquals(p.seriesname, name_data['seriesname'])
+        #end cur_test
+
+        cur_test.description = cdata['description']
+        yield cur_test
 
 
 def check_case(curtest):
