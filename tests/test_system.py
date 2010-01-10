fcb44cdc4c14bbca46ea4ea86bc54a2cf3f8e54d
dbr <dbr.onix@gmail.com>
2010-01-10 15:03:05 +0000

    Test that a config at ~/.tvnamer.xml file does not exist, as it can cuase tests to fail incorrectly

---
+++ tests/test_system.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#encoding:utf-8
+#author:dbr/Ben
+#project:tvnamer
+#repository:http://github.com/dbr/tvnamer
+#license:Creative Commons GNU GPL v2
+# http://creativecommons.org/licenses/GPL/2.0/
+
+"""Tests the current system for things that might cause problems
+"""
+
+import os
+
+def test_nosavedconfig():
+    """A config at ~/.tvnamer.xml could cause problems with some tests
+    """
+    assert not os.path.isfile(os.path.expanduser("~/.tvnamer.xml")), "~/.tvnamer.xml exists, which could cause problems with tests"
