72d6afc3cd02e0b661f3c604684793694b32eeff
dbr <dbr.onix@gmail.com>
2010-01-27 21:39:10 +0000

    Move test_simple_multiple_files to test_functional, as it doesn't use the config

--- tests/test_configfunctional.py
+++ tests/test_configfunctional.py
@@ -25,28 +25,6 @@ def test_simple_single_file():
     verify_out_data(out_data, expected_files)
 
 
-def test_simple_multiple_files():
-    """Tests simple usage with multiple files
-    """
-    input_files = [
-        'scrubs.s01e01.hdtv.fake.avi',
-        'my.name.is.earl.s01e01.fake.avi',
-        'a.fake.show.s12e24.fake.avi',
-        'total.access.s01e01.avi']
-
-    expected_files = [
-        'Scrubs - [01x01] - My First Day.avi',
-        'My Name Is Earl - [01x01] - Pilot.avi',
-        'a fake show - [12x24].avi',
-         'Total Access 24_7 - [01x01] - Episode #1.avi']
-
-    out_data = run_tvnamer(
-        with_files = input_files,
-        with_input = "y\n1\ny\n1\ny\n1\ny\ny\n")
-
-    verify_out_data(out_data, expected_files)
-
-
 def test_batchconfig():
     """Test configured batch mode works
     """
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -12,9 +12,33 @@
 from functional_runner import run_tvnamer, verify_out_data
 
 
+def test_simple_multiple_files():
+    """Tests simple interactive usage with multiple files
+    """
+
+    input_files = [
+        'scrubs.s01e01.hdtv.fake.avi',
+        'my.name.is.earl.s01e01.fake.avi',
+        'a.fake.show.s12e24.fake.avi',
+        'total.access.s01e01.avi']
+
+    expected_files = [
+        'Scrubs - [01x01] - My First Day.avi',
+        'My Name Is Earl - [01x01] - Pilot.avi',
+        'a fake show - [12x24].avi',
+         'Total Access 24_7 - [01x01] - Episode #1.avi']
+
+    out_data = run_tvnamer(
+        with_files = input_files,
+        with_input = "y\n1\ny\n1\ny\n1\ny\ny\n")
+
+    verify_out_data(out_data, expected_files)
+
+
 def test_simple_batch_functionality():
     """Tests renaming single files at a time, in batch mode
     """
+
     tests = [
         {'in':'scrubs.s01e01.hdtv.fake.avi',
         'expected':'Scrubs - [01x01] - My First Day.avi'},
