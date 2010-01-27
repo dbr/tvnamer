d3162c113df1cc2b51a9d1607e8c631b3cee7418
dbr <dbr.onix@gmail.com>
2010-01-27 00:19:10 +0000

    Use test-generator to splitup output. Fix "a fake show" test for new not-renaming-on-error --batch function.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -12,8 +12,8 @@
 from functional_runner import run_tvnamer, verify_out_data
 
 
-def test_simple_functionality():
-    """Tests renaming single files at a time
+def test_simple_batch_functionality():
+    """Tests renaming single files at a time, in batch mode
     """
     tests = [
         {'in':'scrubs.s01e01.hdtv.fake.avi',
@@ -21,14 +21,18 @@ def test_simple_functionality():
         {'in':'my.name.is.earl.s01e01.fake.avi',
         'expected':'My Name Is Earl - [01x01] - Pilot.avi'},
         {'in':'a.fake.show.s12e24.fake.avi',
-        'expected':'a fake show - [12x24].avi'},
+        'expected':'a.fake.show.s12e24.fake.avi'},
         {'in': 'total.access.s01e01.avi',
         'expected': 'Total Access 24_7 - [01x01] - Episode #1.avi'},
     ]
 
     for curtest in tests:
-        out_data = run_tvnamer(
-            with_files = [curtest['in'], ],
-            with_flags = ['--batch'],
-        )
-        verify_out_data(out_data, [curtest['expected'], ])
+        def _the_test():
+            out_data = run_tvnamer(
+                with_files = [curtest['in'], ],
+                with_flags = ['--batch'],
+            )
+            verify_out_data(out_data, [curtest['expected'], ])
+
+        _the_test.description = "test_simple_functionality_%s" % curtest['in']
+        yield _the_test
