4caebd22630c0e0e164e419b38d270cd5388db82
dbr <dbr.onix@gmail.com>
2010-02-24 18:25:19 +0000

    Add more tests for moving files

--- tests/test_movingfiles.py
+++ tests/test_movingfiles.py
@@ -62,3 +62,67 @@ def test_cli_destination():
     expected_files = ['season 1/Scrubs - [01x01] - My First Day.avi']
 
     verify_out_data(out_data, expected_files)
+
+def test_move_interactive_allyes():
+    """Tests interactive UI for moving all files
+    """
+
+    conf = """
+    {"move_files_enable": true,
+    "move_files_destination": "test",
+    "select_first": true}
+    """
+
+    out_data = run_tvnamer(
+        with_files = ['scrubs.s01e01.avi', 'scrubs.s01e02.avi'],
+        with_config = conf,
+        with_input = "y\ny\ny\ny\n")
+
+    expected_files = ['test/Scrubs - [01x01] - My First Day.avi',
+        'test/Scrubs - [01x02] - My Mentor.avi']
+
+    verify_out_data(out_data, expected_files)
+
+def test_move_interactive_allno():
+    """Tests interactive UI allows not moving any files
+    """
+
+    conf = """
+    {"move_files_enable": true,
+    "move_files_destination": "test",
+    "select_first": true}
+    """
+
+    out_data = run_tvnamer(
+        with_files = ['scrubs.s01e01.avi', 'scrubs.s01e02.avi'],
+        with_config = conf,
+        with_input = "y\nn\ny\nn\n")
+
+    expected_files = ['Scrubs - [01x01] - My First Day.avi',
+        'Scrubs - [01x02] - My Mentor.avi']
+
+    verify_out_data(out_data, expected_files)
+
+def test_move_interactive_allno():
+    """Tests interactive UI allows not renaming some files, renaming/moving others
+
+    Rename and move first file, don't rename second file (so no move), and
+    rename but do not move last file (Input is: y/y, n, y/n)
+    """
+
+    conf = """
+    {"move_files_enable": true,
+    "move_files_destination": "test",
+    "select_first": true}
+    """
+
+    out_data = run_tvnamer(
+        with_files = ['scrubs.s01e01.avi', 'scrubs.s01e02.avi', 'scrubs.s01e03.avi'],
+        with_config = conf,
+        with_input = "y\ny\nn\ny\nn\n")
+
+    expected_files = ['test/Scrubs - [01x01] - My First Day.avi',
+        'scrubs.s01e02.avi',
+        'Scrubs - [01x03] - My Best Friend\'s Mistake.avi']
+
+    verify_out_data(out_data, expected_files)
