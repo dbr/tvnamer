021df06951d4e941299438db0d3706c1ca45c61d
dbr <dbr.onix@gmail.com>
2010-04-01 23:24:03 +1030

    Test that the move files directory doesn't create invalid folder names like `CSI: Miami/CSI_ Miami - [01x01].avi`

--- tests/test_movingfiles.py
+++ tests/test_movingfiles.py
@@ -126,3 +126,22 @@ def test_move_interactive_somefiles():
         'Scrubs - [01x03] - My Best Friend\'s Mistake.avi']
 
     verify_out_data(out_data, expected_files)
+
+
+def test_with_invalid_seriesname():
+    """Tests series name containing invalid filename characters
+    """
+
+    conf = """
+    {"move_files_enable": true,
+    "move_files_destination": "%(seriesname)s",
+    "batch": true}
+    """
+
+    out_data = run_tvnamer(
+        with_files = ['csi.miami.s01e01.avi'],
+        with_config = conf)
+
+    expected_files = ['CSI_ Miami/CSI_ Miami - [01x01] - Golden Parachute.avi']
+
+    verify_out_data(out_data, expected_files)
