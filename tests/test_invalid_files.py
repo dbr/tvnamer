545905d2aa8a72a0d9c9028992d21f7e869f5d2d
dbr <dbr.onix@gmail.com>
2011-01-08 13:02:33 +1030

    Update test for [#69], remove skip

--- tests/test_invalid_files.py
+++ tests/test_invalid_files.py
@@ -11,7 +11,6 @@
 
 from functional_runner import run_tvnamer, verify_out_data
 from nose.plugins.attrib import attr
-from nose import SkipTest
 
 
 @attr("functional")
@@ -19,12 +18,10 @@ def test_simple_single_file():
     """Files without series name should be skipped
     """
 
-    raise SkipTest("TODO")
-
     out_data = run_tvnamer(
         with_files = ['S01E02 - Some File.avi'],
         with_flags = ["--batch"])
 
     expected_files = ['S01E02 - Some File.avi']
 
-    verify_out_data(out_data, expected_files)
+    verify_out_data(out_data, expected_files, expected_returncode = 2)
