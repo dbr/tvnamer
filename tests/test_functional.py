dd5850e19155fc0c1238353aa60f9bbca92e654a
dbr <dbr.onix@gmail.com>
2011-08-07 12:59:35 +0930

    Ensure that output-replacements are considered when checking if the existing filename is correct (from lighthouse issue [#81])

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -237,3 +237,33 @@ def test_no_seasonnumber():
     expected_files = ['Scrubs - [01] - My First Day.avi']
 
     verify_out_data(out_data, expected_files)
+
+
+@attr("functional")
+def test_skipping_after_replacements():
+    """When custom-replacement is specified, should still skip file if name is correct
+    """
+
+    conf = """
+    {"select_first": true,
+    "input_filename_replacements": [
+        {"is_regex": false,
+        "match": "v",
+        "replacement": "u"}
+    ],
+    "output_filename_replacements": [
+        {"is_regex": false,
+        "match": "u",
+        "replacement": "v"}
+    ]
+    }
+    """
+
+    out_data = run_tvnamer(
+        with_files = ['Scrvbs - [01x01] - My First Day.avi'],
+        with_config = conf,
+        with_input = "")
+
+    expected_files = ['Scrvbs - [01x01] - My First Day.avi']
+
+    verify_out_data(out_data, expected_files)
