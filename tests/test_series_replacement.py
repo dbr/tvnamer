6f397acd8a1544594d24ca89160642e7c8b9afb2
dbr <dbr.onix@gmail.com>
2012-01-26 17:48:33 +1030

    Tests for series-replacement

---
+++ tests/test_series_replacement.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+
+"""Tests custom replacements on input/output files
+"""
+
+from functional_runner import run_tvnamer, verify_out_data
+from nose.plugins.attrib import attr
+
+
+@attr("functional")
+def test_replace_input():
+    """Tests replacing strings in input files
+    """
+    out_data = run_tvnamer(
+        with_files = ['scruuuuuubs.s01e01.avi'],
+        with_config = """
+{
+    "input_series_replacements": {
+        "scru*bs": "scrubs"},
+    "always_rename": true,
+    "select_first": true
+}
+""")
+
+    expected_files = ['Scrubs - [01x01] - My First Day.avi']
+
+    verify_out_data(out_data, expected_files)
+
+
+@attr("functional")
+def test_replace_output():
+    """Tests replacing strings in input files
+    """
+    out_data = run_tvnamer(
+        with_files = ['Scrubs.s01e01.avi'],
+        with_config = """
+{
+    "output_series_replacements": {
+        "Scrubs": "Replacement Series Name"},
+    "always_rename": true,
+    "select_first": true
+}
+""")
+
+    expected_files = ['Replacement Series Name - [01x01] - My First Day.avi']
+
+    verify_out_data(out_data, expected_files)
