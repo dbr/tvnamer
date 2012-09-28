5d2129b6ae4f27bbee7a84a54f3f38d3a7239f9f
Doug Beck <doug@douglasbeck.com>
2012-09-28 11:59:22 +0700

    Modification of filename_blacklist behavior.
    filename_blacklist should be able to accept a simple string
    specifying an exact filename you would like to ignore.
    
    example:
    "filename_blacklist": [".DS_Store", "Thumbs.db"],
    
    I believe this is a more intuitive use of a filename blacklist and the
    ability to add a dict with advanced functionality still exists.

--- tvnamer/utils.py
+++ tvnamer/utils.py
@@ -194,8 +194,12 @@ class FileFinder(object):
         """Checks if the filename (optionally excluding extension)
         matches filename_blacklist
 
-        self.with_blacklist should be a list of dicts, where each dict
-        contains:
+        self.with_blacklist should be a list of strings and/or dicts:
+
+        a string, specifying a specific file name to ignore
+        "filename_blacklist": [".DS_Store", "Thumbs.db"], 
+
+        a dictionary, where each dict contains:
 
         Key 'match' - (if the filename matches the pattern, the filename
         is blacklisted)
@@ -218,6 +222,11 @@ class FileFinder(object):
         fname, fext = os.path.splitext(fullname)
 
         for fblacklist in self.with_blacklist:
+            if isinstance(fblacklist, basestring):
+                if fullname == fblacklist:
+                    return True
+                else: continue
+            
             if "full_path" in fblacklist and fblacklist["full_path"]:
                 to_check = filepath
             else:
