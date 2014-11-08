4a139bd78bed5dd01194bce2a91805912412d32e
dbr <dbr.onix@gmail.com>
2014-11-09 00:03:22 +1030

    Add relevant Python version classifiers to setup.py

--- setup.py
+++ setup.py
@@ -57,5 +57,11 @@ classifiers=[
     "Programming Language :: Python",
     "Topic :: Multimedia",
     "Topic :: Utilities",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
 ],
 )
