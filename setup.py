1c4e3feed9a64c60ef0d149f230257dff64ee20b
dbr <dbr.onix@gmail.com>
2016-10-10 19:15:31 +1030

    Update classifier to mention python 3.5 also

--- setup.py
+++ setup.py
@@ -85,5 +85,6 @@ classifiers=[
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
 ],
 )
