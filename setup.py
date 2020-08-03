c9dc9e0d08ff1edcf2dd8180ee896a3f90a54b39
dbr <dbr.onix@gmail.com>
2020-08-03 21:09:03 +0930

    Update setup.py to reflect no more 2.7 [#186]

--- setup.py
+++ setup.py
@@ -49,12 +49,9 @@ classifiers=[
     "Topic :: Multimedia",
     "Topic :: Utilities",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
 ],
 )
