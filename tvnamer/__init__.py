df284306268291a52fcf5b0af2bfdfdd2a9aad12
dbr <dbr.onix@gmail.com>
2020-09-19 19:06:34 +0930

    Bump version

--- tvnamer/__init__.py
+++ tvnamer/__init__.py
@@ -6,7 +6,7 @@ Uses data from www.thetvdb.com (via tvdb_api) to rename TV episode files from
 "some.show.name.s01e01.blah.avi" to "Some Show Name - [01x01] - The First.avi"
 """
 
-__version__ = "3.0"
+__version__ = "3.0.1"
 __author__ = "dbr/Ben"
 
 import os
